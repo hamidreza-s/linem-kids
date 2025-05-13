f6aace66c429a61488bd88d34e567413d4ef944d
Hamidreza Soleimani <hamidreza.s@gmail.com>
2025-05-13 22:32:04 +0200

    Add new icons and web manifest: Include Android and Apple touch icons, favicons, and a web manifest for improved branding and PWA support in the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import { Inter } from "next/font/google"
 import Provider from "@/components/ui/provider"
+import { Metadata } from 'next'
 
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
 })
+
+export const metadata: Metadata = {
+  title: 'Linem',
+}
 
 export default function RootLayout({
   children,
@@ -13,7 +18,14 @@ export default function RootLayout({
 }) {
   return (
     <html className={inter.className} suppressHydrationWarning>
-      <head />
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className="bg-white min-h-screen" style={{
         backgroundImage: `
           linear-gradient(to right, #e5e7eb 1px, transparent 1px),
